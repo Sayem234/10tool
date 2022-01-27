632b66f7633ef7b7a9b88ea6a40ac69d438d9665
Sayem234 <88239049+Sayem234@users.noreply.github.com>
2022-01-27 23:31:30 +0600

    Add files via upload

---
+++ All.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python2
+# -*- coding: utf-8
+#Coded by SaYeM
+
+
+import os,time,sys
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+logo="""
+\033[1;32m
+ S)ssss    A)aa   Y)    yy E)eeeeee  M)mm mmm  
+S)    ss  A)  aa   Y)  yy  E)       M)  mm  mm 
+ S)ss    A)    aa   Y)yy   E)eeeee  M)  mm  mm 
+     S)  A)aaaaaa    Y)    E)       M)  mm  mm 
+S)    ss A)    aa    Y)    E)       M)      mm 
+ S)ssss  A)    aa    Y)    E)eeeeee M)      mm"""
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(3.0 / 200)
+os.system("clear")
+print logo
+os.system("xdg-open https://facebook.com/groups/196310582274445/")
+os.system("xdg-open https://facebook.com/groups/196310582274445/")
+os.system("clear")
+print logo
+psb('============================================')
+print""
+print("input Tool username && password ").center(50)
+print""
+print""
+CorrectUsername = "S4M"
+CorrectPassword = "10TOOL"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("\033[1;96m[] \x1b[1;93mUsername Of Tool \x1b[1;96m: ")
+    if (username == CorrectUsername):
+    	password = raw_input("\033[1;96m[] \x1b[1;93mPassword Of Tool \x1b[1;96m: ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username
+            loop = 'false'
+        else:
+            print "Wrong Password"
+            os.system('xdg-open https://www.facebook.com/junide.hossin.92')
+    else:
+        print "Wrong Username"
+        os.system('xdg-open https://www.facebook.com/junide.hossin.92')
+os.system("clear")
+print logo
+print""
+jalan('\033[1;96mAuthor       \033[1;93m: \033[1;92mSAYEM')
+jalan('\033[1;96mGithub       \033[1;93m: \033[1;92mhttps://github.com/Sayem234')
+jalan('\033[1;96mFacebook     \033[1;93m: \033[1;92mwww.facebook.com/junide.hossin.92')
+jalan('\033[1;96mPage  	     \033[1;93m: \033[1;92mTermux Tips && Trick')
+print""
+jalan("🔸🔹🔸🔹🔸🔹🔸🔹🔸🔹S|A|Y|E|M🔹🔸🔹🔸🔹🔸🔹🔸🔹🔸")
+jalan("🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾🚾")
+jalan("🔸🔹🔸🔹🔸🔹🔸🔹🔸🔹🔸🔹🔸🔹🔸🔹🔸🔹🔸🔹🔸🔹")
+print""
+print("Best 10 Cloning Tool ").center(50)
+print("            ◄◽◽◽◽◽◽◽◽◽◽◽▶").center(50)
+print""
+psb("\033[1;93m[1]Sv1-Author:SaYem")
+psb("\033[1;92m[2]Shieldpro-Author:Abdullah")
+psb("\033[1;96m[3]Apro-Author:Annos")
+psb("\033[1;93m[4]premium-Author:Rozak")
+psb("\033[1;92m[5]Elite-Author:Elite")
+psb("\033[1;96m[6]NI-11(Bangladeshi random id cloning)-Author:NazMul")
+psb("\033[1;93m[7]UID clone-Author:KowShik")
+psb("\033[1;92m[8]bypass07-Author:Ubaid")
+psb("\033[1;96m[9]indocrack(Indian random id cloning)-Author:Tahmid")
+psb("\033[1;93m[10]pakcrack(Pakistani random id cloning)-Author:Tahmid")
+print""
+psb("\033[1;91m--------------------------------------------------")
+psb("\033[1;99m		Select an Option ")
+
+select = raw_input('\x1b[92;1m√SELECT:) ')
+if select == '1':
+   os.system("git clone https://github.com/Sayem234/Sv1 && cd Sv1 && python2 Sv1.py")
+elif select =='2':
+	os.system("git clone https://github.com/AbdullahDada420/shieldpro && cd shieldpro && python2 Abdullah.py")
+elif select =='3':
+	os.system("git clone https://github.com/THE-DEMON-ANNOS/APRO.git && cd APRO && python2 APRO.py")
+elif select =='4':
+	os.system("git clone https://github.com/RozhakXD/Premium && cd Premium && python Prem.py")
+elif select =='5':
+	os.system("git clone https://github.com/AbdullahDada420/elite && cd elite && python elite.py")
+elif select =='6':
+	psb("\033[1;91mUsername Of this Tool is: 11digit")
+	psb("\033[1;91mPassWord of this Tool is : NAZMUL")
+	print""
+	psb("\033[1;91mPlease kindly Note it.You Need this in this tool")
+	time.sleep(1)
+	os.system("clear")
+	os.system("git clone https://github.com/DARK-NI/Fb-Clonner-Ni11 && chmod +x * && cd && Fb-Clonner-Ni11 && python2 11digit.py")
+elif select =='7':
+	os.system("git clone https://github.com/Kowshik-Bhai/Uid-Clone && cd Uid-Clone && python2 Kowshik-Bhai.py")
+elif select =='8':
+	os.system("git clone https://github.com/bypass07/ubaid07 && cd ubaid07 && python2 ubaid07.py")
+elif select =='9':
+	psb("\033[1;91mUsername Of this Tool is: cra3kr")
+	psb("\033[1;91mPassWord of this Tool is : tahmidrayat")
+	print""
+	psb("\033[1;91mPlease kindly Note it.You Need this in this tool")
+	time.sleep(1)
+	os.system("clear")
+	os.system("git clone https://github.com/htr-tech/indocrack.git && cd indocrack && python2 crack.py")
+elif select =='10':
+	psb("\033[1;91mUsername Of this Tool is: cra3kr")
+	psb("\033[1;91mPassWord of this Tool is : htrtech")
+	print""
+	psb("\033[1;91mPlease kindly Note it.You Need this in this tool")
+	time.sleep(1)
+	os.system("clear")
+	os.system("git clone https://github.com/htr-tech/pakcrack.git && cd pakcrack && python2 crack.py")
+else:
+        print ''
+        print ('\t\033[1;32mYOU CANNOT SELECT A VALID OPTION')
+        psb("\t\033[1;94m~~~~~~~~~~\033[1;32mRun it Again\033[1;94m~~~~~~~~~~")
+        print ''
+
+   
